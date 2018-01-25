8c8dc0f742ec06a08aebddc326a5e87b43cfb35a
Prikot <a.prikotov@gmail.com>
2018-01-25 11:33:27 +0300

    tissue page 5%

--- source/js/owl-carousel.js
+++ source/js/owl-carousel.js
@@ -4,26 +4,19 @@
 (function(){
   $('#slider-about').owlCarousel({
     loop:true, //Зацикливаем слайдер
-    // margin:10, //Отступ от элемента справа в 50px
+    margin:5, //Отступ от элемента справа в 5px
     nav: true, //Отключение навигации
     animateOut: 'fadeOut',
     dots: false,
     navText: ["&#9664;" , "&#9654;"],
-    autoplay:true, //Автозапуск слайдера
+    autoplay:false, //Автозапуск слайдера
     smartSpeed:1000, //Время движения слайда
     autoplayTimeout:4000, //Время смены слайда
     responsive:{ //Адаптивность. Кол-во выводимых элементов при определенной ширине.
       0:{
         items:1
-      },
-      600:{
-        items:1
-      },
-      1000:{
-        items:1
       }
     }
-
       });
 })();
 
@@ -31,7 +24,7 @@
 (function(){
   $('#slider-tissue').owlCarousel({
     loop:true, //Зацикливаем слайдер
-    // margin:10, //Отступ от элемента справа в 50px
+    margin:5, //Отступ от элемента справа в 5px
     nav: true, //Отключение навигации
     animateOut: 'fadeOut',
     dots: false,
@@ -42,15 +35,8 @@
     responsive:{ //Адаптивность. Кол-во выводимых элементов при определенной ширине.
       0:{
         items:1
-      },
-      600:{
-        items:1
-      },
-      1000:{
-        items:1
       }
     }
-
       });
 })();
 
@@ -58,22 +44,16 @@
 (function(){
   $('#slider-studio').owlCarousel({
     loop:true, //Зацикливаем слайдер
-    // margin:10, //Отступ от элемента справа в 50px
+    margin: 5, //Отступ от элемента справа в 5px
     nav: true, //Отключение навигации
     animateOut: 'fadeOut',
     dots: false,
     navText: ["&#9664;" , "&#9654;"],
-    autoplay:true, //Автозапуск слайдера
+    autoplay:false, //Автозапуск слайдера
     smartSpeed:1000, //Время движения слайда
     autoplayTimeout:4000, //Время смены слайда
     responsive:{ //Адаптивность. Кол-во выводимых элементов при определенной ширине.
       0:{
-        items:1
-      },
-      600:{
-        items:1
-      },
-      1000:{
         items:1
       }
     }
@@ -84,7 +64,7 @@
 (function(){
   $('#slider-showroom').owlCarousel({
     loop:true, //Зацикливаем слайдер
-    // margin:10, //Отступ от элемента справа в 50px
+    margin:5, //Отступ от элемента справа в 5px
     nav: true, //Отключение навигации
     animateOut: 'fadeOut',
     dots: false,
@@ -95,25 +75,7 @@
     responsive:{ //Адаптивность. Кол-во выводимых элементов при определенной ширине.
       0:{
         items:1
-      },
-      600:{
-        items:1
-      },
-      1000:{
-        items:1
       }
     }
-
   });
 })();
-
-
-//
-// $('#about').mouseenter(function(){
-//   $('.owl-carousel').trigger('play.owl.autoplay');
-// });
-//
-// $('#about').mouseleave(function(){
-//   $('.owl-carousel').trigger('stop.owl.autoplay');
-// });
-
