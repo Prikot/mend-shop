a1a8fd36871691c3c6cbb638026e1dfed1431861
Prikot <a.prikotov@gmail.com>
2018-02-03 00:25:22 +0300

    instagram

--- gulp/paths/js.foundation.js
+++ gulp/paths/js.foundation.js
@@ -2,5 +2,6 @@
 
 module.exports = [
   './node_modules/jquery/dist/jquery.min.js',
-  './node_modules/owl.carousel/dist/owl.carousel.min.js'
+  './node_modules/owl.carousel/dist/owl.carousel.min.js',
+  './node_modules/instafeed/lib/instafeed.js'
 ];
--- source/js/app.js
+++ source/js/app.js
@@ -1,4 +1,13 @@
+var instafeed = new Instafeed({
+  userId: '4772247690',
+  limit: 6,
+  accessToken: '4772247690.c50ba1c.563e63cb6ceb4501b0e9c20918749729',
+  template: 'a(href="https://www.instagram.com/tkani_mendosa/")' +  'img(src="{{image}}" style="margin: 0 10px 0 0; width: 120px; height: 120px;")'
+});
+
+instafeed.run();
+
+
 // (function(){
-//   $('.owl-carousel').owlCarousel();
-// })();
 //
+// })();
