6b2aeacbcec915bce4a3cfa0abc68159b6da59df
Prikot <a.prikotov@gmail.com>
2018-02-19 01:42:37 +0300

    fix insta issue

--- source/js/app.js
+++ source/js/app.js
@@ -1,3 +1,18 @@
+// var express = require('express');
+// var app = express();
+// app.get('/', function (req, res) {
+//   res.send('Hello World!'
+//   );
+// });
+// app.listen(
+//     3000
+//     , function () {
+//       console.log(
+//           'Example app listening on port 3000!'
+//       );
+//     });
+
+
 var instafeed = new Instafeed({
   userId: '4772247690',
   limit: 6,
--- source/js/banner-change.js
+++ source/js/banner-change.js
@@ -1,33 +1,37 @@
-// /**
-//  * Created by aleksandr on 26.01.18.
-//  */
-//
+// // /**
+// //  * Created by aleksandr on 26.01.18.
+// //  */
+// //
 // (function(){
 //
-//   var imgHead = [
-//     'assets/img/banner-1.png',
-//     'assets/img/banner-2_2.png'
-//     // 'images/imgCsaHead/img2.jpg',
-//     // 'images/imgCsaHead/img3.jpg'
-//   ], i=1;
-//
-//   var block = $('#header');
+//   var block = $('#header .header__background');
+//   var item = $('#header .header__background .main-pic');
 //
 //   setInterval(function() {
-//     if(i > (imgHead.length-1)){
-//       block.animate({'opacity':'0'}, 1000,  function(){
-//         i=1;
-//         block.css({'background':'url('+imgHead[0]+')'});
-//       });
-//       block.animate({'opacity':'1'}, 1000);
-//     }else{
-//       block.animate({'opacity':'0'}, 200, function(){
-//         block.css({'background':'url('+imgHead[i]+')'});
-//         i++;
-//       });
-//       block.animate({'opacity':'1'},200);
+//     for(var i = 0;  i < item.length; i++) {
+//       if(i < item.length){
+//         block.animate({'opacity':'0'}, 1000,  function(){
+//           i=1;
+//           block.css({'background':'url('+imgHead[0]+')'});
+//         });
+//         block.animate({'opacity':'1'}, 1000);
+//       }else{
+//         block.animate({'opacity':'0'}, 200, function(){
+//           block.css({'background':'url('+imgHead[i]+')'});
+//           i++;
+//         });
+//         block.animate({'opacity':'1'},200);
+//       }
 //     }
+//
+//
 //
 //   }, 8000);
 // })();
+//
+//
+// $(function () {}
+//
+//
+// );
 //
