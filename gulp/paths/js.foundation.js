dcd3942745fc8ee7e10c72fedfcc39d2053141f4
Prikot <a.prikotov@gmail.com>
2018-03-16 12:56:08 +0300

    first relize 16 of march

--- gulp/paths/js.foundation.js
+++ gulp/paths/js.foundation.js
@@ -3,5 +3,7 @@
 module.exports = [
   './node_modules/jquery/dist/jquery.min.js',
   './node_modules/owl.carousel/dist/owl.carousel.min.js',
-  './node_modules/instafeed/lib/instafeed.js'
+  './node_modules/instafeed/lib/instafeed.js',
+  './node_modules/instafeed.js/instafeed.js'
+
 ];
--- source/js/app.js
+++ source/js/app.js
@@ -25,12 +25,15 @@ $.fn.preload = function() {
 // ==============================
 // INSTAGRAM FOOTER
 // ==============================
+// https://api.instagram.com/oauth/authorize/?client_id=36a7ffd38682428ba1e245c711447831&redirect_uri=REDIRECT-URI&response_type=token
 
 var instafeed = new Instafeed({
+  get: 'user',
   userId: '4772247690',
+  // clientId: '36a7ffd38682428ba1e245c711447831',
   limit: 6,
-  accessToken: '4772247690.c50ba1c.563e63cb6ceb4501b0e9c20918749729',
-  template: 'a(href="https://www.instagram.com/tkani_mendosa/")' +  'img(src="{{image}}" style="margin: 0 10px 0 0; width: 120px; height: 120px;")'
+  accessToken: '4772247690.36a7ffd.d85222567ab549dc9e75872185b1e355'
+  // template: 'a(href="https://www.instagram.com/tkani_mendosa/")' +  'img(src="{{image}}" style="margin: 0 10px 0 0; width: 120px; height: 120px;")'
 });
 
 instafeed.run();
@@ -61,7 +64,24 @@ $(function() {
 
 $(function() {
   $('.barhat__img').on('click', function(e) {
+
     e.preventDefault();
+    // $('.barhat__img').removeClass('active');
+    // $(this).toggleClass('active');
     $(this).$('.barhat__header').animate({ left: 0}, 1000, 'linear');
   });
 });
+
+// if (window.innerWidth <= 480 ) {
+//   var a=document.getElementById('header__slide_1');
+//   var b=document.getElementById('header__slide_2');
+//   var c=document.getElementById('header__slide_3');
+//   var d=document.getElementById('header__slide_4');
+//   var e=document.getElementById('header__slide_5');
+//
+//   a.src=a.src.replace('/assets/img/banner/banner-17.jpg','/assets/img/banner/banner-17-900.jpg');
+//   b.src=b.src.replace('/assets/img/banner/banner-13.jpg','/assets/img/banner/banner-13-900.jpg');
+//   c.src=c.src.replace('/assets/img/banner/banner-14.jpg','/assets/img/banner/banner-14-900.jpg');
+//   d.src=d.src.replace('/assets/img/banner/banner-18.jpg','/assets/img/banner/banner-18-900.jpg');
+//   e.src=e.src.replace('/assets/img/banner/banner-11.jpg','/assets/img/banner/banner-11-900.jpg');
+// }
